253bb032ef3a0e407798b61f04fab501eca7631f
Lokesh Kumar <lokeshkumar.080806@gmail.com>
2025-03-12 12:01:48 +0530

    solving deployment issue

--- app/page.tsx
+++ app/page.tsx
@@ -5,24 +5,22 @@ import Footer from "@/components/footer";
 import Hero from "@/components/hero";
 import { Navbar } from "@/components/navbar";
 import Testimonial from "@/components/testimonial";
-import Lenis from "lenis";
+import { ReactLenis, useLenis } from 'lenis/react'
 
 export default function Home() {
-  const lenis = new Lenis({
-    autoRaf: true,
-  });
 
+  const lenis = useLenis()
   console.log(lenis);
   
 
   return (
-    <>
+    <ReactLenis root>
       <Navbar />
       <Hero />
       <Features />
       <FAQ />
       <Testimonial />
       <Footer />
-    </>
+    </ReactLenis>
   );
 }
