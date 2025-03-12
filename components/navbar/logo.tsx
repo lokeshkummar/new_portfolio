5daba924a5cbdb4aae7da84b06aa5716dc961b4f
Lokesh Kumar <lokeshkumar.080806@gmail.com>
2025-03-12 11:41:35 +0530

    fixing import issue

--- components/navbar/logo.tsx
+++ components/navbar/logo.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import logo from "../../../shadcn-ui-landing-page/public/logo.png"
+import logo from "../../public/logo.png"
 
 export const Logo = () => (
   <Image
