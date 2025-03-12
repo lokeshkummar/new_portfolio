220d35950a946eff15c3fc7b5a64c82ca3bac8d3
Lokesh Kumar <lokeshkumar.080806@gmail.com>
2025-03-12 12:52:57 +0530

    add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,45 @@ const geistSans = Geist({
 
 export const metadata: Metadata = {
   title: "Lokesh's Portfolio",
+  icons: [
+    {
+      rel: "icon",
+      url: "/favicon.ico",
+    },
+    {
+      rel: "apple-touch-icon",
+      url: "/apple-touch-icon.png",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      url: "/favicon-32x32.png",
+      sizes: "32x32",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      url: "/favicon-16x16.png",
+      sizes: "16x16",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      url: "/android-chrome-192x192.png",
+      sizes: "192x192",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      url: "/android-chrome-512x512.png",
+      sizes: "512x512",
+    },
+  ],
+  robots: {
+    index: true,
+    follow: true,
+  },
+  manifest: "/site.webmanifest",
   }
 
 export default function RootLayout({
